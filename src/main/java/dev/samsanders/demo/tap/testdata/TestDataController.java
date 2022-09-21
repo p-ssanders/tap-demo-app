d5260f95d07b1b5fea35fef82632f619a6272759
Sam Sanders <ssanders@vmware.com>
2022-09-21 16:33:11 -0600

    publish events

--- src/main/java/dev/samsanders/demo/tap/testdata/TestDataController.java
+++ src/main/java/dev/samsanders/demo/tap/testdata/TestDataController.java
@@ -1,5 +1,6 @@
 package dev.samsanders.demo.tap.testdata;
 
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,9 +16,11 @@ import java.util.UUID;
 public class TestDataController {
 
     private final TestDataRepository testDataRepository;
+    private final StreamBridge streamBridge;
 
-    public TestDataController(TestDataRepository testDataRepository) {
+    public TestDataController(TestDataRepository testDataRepository, StreamBridge streamBridge) {
         this.testDataRepository = testDataRepository;
+        this.streamBridge = streamBridge;
     }
 
     @GetMapping
@@ -29,8 +32,9 @@ public class TestDataController {
     ResponseEntity<Void> createTestData() {
         TestData testData = new TestData(0, UUID.randomUUID().toString(), null);
         testData = testDataRepository.create(testData);
-
         UriComponents location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(testData.id());
+
+        streamBridge.send("test-data-created", Collections.singletonMap("URI", location.toString()));
 
         return ResponseEntity.created(location.toUri()).build();
     }
